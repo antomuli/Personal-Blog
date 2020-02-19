9c98db85831715ddd9d16c63fa63466bcbd86fe2
Anthony Muli <mulianthony561@gmail.com>
2020-02-20 00:56:34 +0300

    Registered the blueprint

---
+++ app/__init__.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_simplemde import SimpleMDE
+from flask_mail import Mail
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+photos = UploadSet('photos', IMAGES)
+
+mail = Mail()
+
+simple = SimpleMDE()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+
+    configure_uploads(app, photos)
+
+    return app
