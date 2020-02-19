c91f94ae6c3c1e9d34755534307f4ec35971fef1
Anthony Muli <mulianthony561@gmail.com>
2020-02-20 00:36:18 +0300

    Defined the function for manager.shell

---
+++ manage.py
@@ -0,0 +1,28 @@
+from app import create_app, db
+from flask_script import Manager, Server
+from app.models import User, Blog, Comment, PhotoProfile
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    '''
+    Run the unit tests
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Blog = Blog, Comment = Comment)
+
+if __name__ == '__main__':
+    manager.run()
