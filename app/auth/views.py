6992c89e890a339152829a55fa04f5908d70f8a5
Anthony Muli <mulianthony561@gmail.com>
2020-02-20 00:58:48 +0300

    Created the auth folder and views.py file inside that particular folder

---
+++ app/auth/views.py
@@ -0,0 +1,46 @@
+from flask import render_template, redirect, url_for, flash, request
+from . import auth
+from ..models import User, Subscriber
+from .forms import SignupForm, LoginForm
+from .. import db
+from flask_login import login_user, logout_user, login_required
+
+
+@auth.route('/login', methods = ["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Blog login"
+
+    return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/register', methods = ["GET", "POST"])
+def register():
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        if form.subscribe.data:
+            subscriber = Subscriber(email = form.email.data, username = form.username.data)
+            db.session.add(subscriber)
+            db.session.commit()
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/signup.html', signup_form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been Successfully logged out')
+    return redirect(url_for("main.index"))
