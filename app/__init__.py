ee1b9409ff45a748da1048723e9c1822e038cec3
Anthony Muli <mulianthony561@gmail.com>
2020-02-20 16:36:10 +0300

    Remove flask uploads from __init__.py.

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_simplemde import SimpleMDE
 from flask_mail import Mail
 
@@ -14,7 +13,6 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
-photos = UploadSet('photos', IMAGES)
 
 mail = Mail()
 
@@ -38,6 +36,6 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
 
-    configure_uploads(app, photos)
+    configure_uploads(app)
 
     return app
