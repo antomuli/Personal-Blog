440ccd5eae02816d2ba0dba266927632f340faaa
Anthony Muli <mulianthony561@gmail.com>
2020-02-20 16:01:40 +0300

    Added routes to ease page navigation

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for, flash, request
 from . import auth
-from ..models import User, Subscriber
+from ..models import User
 from .forms import SignupForm, LoginForm
 from .. import db
 from flask_login import login_user, logout_user, login_required
